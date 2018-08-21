2d67aab86e1ffffddf4a010fe90637c78a099497
Kim Pham <pham.kim.a@gmail.com>
2018-08-21 00:49:48 +0000

    automatically backed up by learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -3,8 +3,9 @@ def levitation_quiz
 	loop do
 	  puts "What is the spell that enacts levitation?"
 	  answer = gets.chomp
-	  if answer = "Wingardium Leviosa"
+	  if answer == "Wingardium Leviosa"
 	    break
+	  end
 	end
 end
 
